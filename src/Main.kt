c3860162471afda851b4d583c8cc6eb4f3606310
Philip Hamilton <pjhamilton4@gmail.com>
2020-09-16 23:11:23 -0400

    Var and val learning

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,24 @@
+val name: String = "Philip"
+val name2: String = "Bobby"
+var greeting: String = "hello"
+
+var nameNull: String? = "Null Test"
+
 fun main(){
-    println("Hello Kotlin")
+    println(nameNull)
+    nameNull = null
+    println(nameNull)
+
+    println("$greeting $name from $name2")
+
+    val name: String = "Phil"
+
+    var name2: String = "Tester"
+
+    println("$greeting $name from $name2")
+
+    name2 = "Tester 2"
+
+    println("$greeting $name from $name2")
+
 }
