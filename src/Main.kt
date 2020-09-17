23d326edea07df3796c7355930652e141c2cb171
Philip Hamilton <pjhamilton4@gmail.com>
2020-09-16 23:16:25 -0400

    variable can be assigned if/else or when

--- src/Main.kt
+++ src/Main.kt
@@ -2,11 +2,9 @@ val name: String = "Philip"
 var greeting: String? = null
 
 fun main(){
-    greeting = "Hello, "
-    when(greeting){
-        null -> println("Hi")
-        else -> println(greeting)
-    }
+    greeting = "Hello"
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+    println(greetingToPrint)
     println(name)
 
 }
