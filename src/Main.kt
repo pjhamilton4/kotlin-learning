1a0533f2143bbc437cf72095f11cf8210db2c71b
Philip Hamilton <pjhamilton4@gmail.com>
2020-09-16 23:13:12 -0400

    if else

--- src/Main.kt
+++ src/Main.kt
@@ -1,24 +1,13 @@
 val name: String = "Philip"
-val name2: String = "Bobby"
-var greeting: String = "hello"
-
-var nameNull: String? = "Null Test"
+var greeting: String? = null
 
 fun main(){
-    println(nameNull)
-    nameNull = null
-    println(nameNull)
-
-    println("$greeting $name from $name2")
-
-    val name: String = "Phil"
-
-    var name2: String = "Tester"
-
-    println("$greeting $name from $name2")
-
-    name2 = "Tester 2"
-
-    println("$greeting $name from $name2")
+    //greeting = "Hello, "
+    if(greeting != null) {
+        println("$greeting")
+    }else{
+        println("Hi, ")
+    }
+    println("$name")
 
 }
